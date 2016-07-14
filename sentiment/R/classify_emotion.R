c281591e9c5a5dfdfd5a13abe1129c72a21158a6
EliF0x <e.claire.fox@gmail.com>
2016-07-14 09:29:35 -0400

    Add files via upload

--- sentiment/R/classify_emotion.R
+++ sentiment/R/classify_emotion.R
@@ -2,12 +2,12 @@ classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FAL
 	matrix <- create_matrix(textColumns,...)
 	lexicon <- read.csv(system.file("data/emotions.csv.gz",package="sentiment"),header=FALSE)
 
-	counts <- list(anger=length(which(lexicon[,2]=="anger")),disgust=length(which(lexicon[,2]=="disgust")),fear=length(which(lexicon[,2]=="fear")),joy=length(which(lexicon[,2]=="joy")),sadness=length(which(lexicon[,2]=="sadness")),surprise=length(which(lexicon[,2]=="surprise")),total=nrow(lexicon))
+	counts <- list(anger=length(which(lexicon[,2]=="anger")),trust=length(which(lexicon[,2]=="trust")),distrust=length(which(lexicon[,2]=="distrust")),fear=length(which(lexicon[,2]=="fear")),joy=length(which(lexicon[,2]=="joy")),sadness=length(which(lexicon[,2]=="sadness")),surprise=length(which(lexicon[,2]=="surprise")),anticipation=length(which(lexicon[,2]=="anticipation"))total=nrow(lexicon))
 	documents <- c()
 
 	for (i in 1:nrow(matrix)) {
 		if (verbose) print(paste("DOCUMENT",i))
-		scores <- list(anger=0,disgust=0,fear=0,joy=0,sadness=0,surprise=0)
+		scores <- list(anger=0,trust=0,distrust=0,fear=0,joy=0,sadness=0,surprise=0,anticipation=0)
 		doc <- matrix[i,]
 		words <- findFreqTerms(doc,lowfreq=1)
 		
@@ -47,10 +47,10 @@ classify_emotion <- function(textColumns,algorithm="bayes",prior=1.0,verbose=FAL
         }
 		
         best_fit <- names(scores)[which.max(unlist(scores))]
-        if (best_fit == "disgust" && as.numeric(unlist(scores[2]))-3.09234 < .01) best_fit <- NA
-		documents <- rbind(documents,c(scores$anger,scores$disgust,scores$fear,scores$joy,scores$sadness,scores$surprise,best_fit))
+        if (best_fit == "distrust" && as.numeric(unlist(scores[2]))-3.09234 < .01) best_fit <- NA
+		documents <- rbind(documents,c(scores$anger,scores$trust,scores$distrust,scores$fear,scores$joy,scores$sadness,scores$surprise,scores$anticipation,best_fit))
 	}
 	
-	colnames(documents) <- c("ANGER","DISGUST","FEAR","JOY","SADNESS","SURPRISE","BEST_FIT")
+	colnames(documents) <- c("ANGER","TRUST", "DISTRUST","FEAR","JOY","SADNESS","SURPRISE","ANTICIPATION","BEST_FIT")
 	return(documents)
 }
